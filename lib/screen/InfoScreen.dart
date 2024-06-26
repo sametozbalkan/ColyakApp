0cdc0b0b0f48f790e42f6db49ebef772ee341af0
sametozbalkan <sametozbalkan@hotmail.com>
2024-06-26 21:13:12 +0300

    v1.0.5+++

--- lib/screen/InfoScreen.dart
+++ lib/screen/InfoScreen.dart
@@ -9,52 +9,61 @@ class InfoScreen extends StatelessWidget {
   static const List<PageData> pages = [
     PageData(
       title: "Uygulama Ana Ekranı",
-      description: "Uygulamaya giriş yapıldığında sizi karşılayacak olan ilk ekran.",
+      description: "Uygulamaya giriş yaptığınızda sizi karşılayan ilk ekran.",
       imageAsset: "assets/images/mainscreen.png",
     ),
     PageData(
       title: "Kolay Erişilebilir Menü",
-      description: "Ana ekranın sol üstünden menüye kolayca erişebilir ve yapmak istediklerinizi seçebilirsiniz.",
+      description:
+          "Ana ekranın sol üst köşesinden menüye kolayca erişebilir ve yapmak istediklerinizi seçebilirsiniz.",
       imageAsset: "assets/images/hamburger.png",
     ),
     PageData(
       title: "Öğün Listeni Oluştur",
-      description: "Öğün listeni oluşturarak bolus için karbonhidrat değerini kolayca belirleyebilirsin.",
+      description:
+          "Öğün listeni oluşturarak bolus için karbonhidrat değerini kolayca belirleyebilirsin.",
       imageAsset: "assets/images/mealscreen.png",
     ),
     PageData(
-      title: "Yüzlerce Yiyecek",
-      description: "Bu listeden özgürce seçim yapabilir ve kolayca listene ekleyebilirsin.",
+      title: "Yüzlerce Tarif ve Gıda",
+      description:
+          "Bu listeden istediğin tarifi ve hazır gıdayı seçebilirsin ve kolayca listene ekleyebilirsin.",
       imageAsset: "assets/images/addmealscreen.png",
     ),
     PageData(
-      title: "Birbirinden Farklı Tarifler",
-      description: "Diyetisyeninin özenle hazırladığı tüm tariflere kolayca erişebilirsin ve favorileyebilirsin.",
+      title: "Çeşit Çeşit Tarifler",
+      description:
+          "Diyetisyeninin özenle hazırladığı tariflere kolayca erişebilirsin ve favorilerine ekleyebilirsin.",
       imageAsset: "assets/images/receipts.png",
     ),
     PageData(
       title: "Geniş Tarif Detayları",
-      description: "İstediğin tarifin malzemelerine, yapılışına ve besin değerlerine kolayca ulaşabilirsin.",
+      description:
+          "İstediğin tarifin malzemelerine, yapılışına ve besin değerlerine kolayca ulaşabilirsin.",
       imageAsset: "assets/images/receiptdetails.png",
     ),
     PageData(
-      title: "Özgürce Yorum Yap",
-      description: "İstediğin tarifin altına bu tarifle ilgili aklında ne geçerse yazabilirsin.",
+      title: "Tariflere Yorum Yap",
+      description:
+          "İstediğin tarifin altına bu tarifle ilgili aklından ne geçerse yazabilirsin.",
       imageAsset: "assets/images/commentscreen.png",
     ),
     PageData(
-      title: "Yorumlar Altında Tartış",
-      description: "İstediğin yoruma cevap verebilir ve yorum sahibiyle tarif hakkında konuşabilirsin.",
+      title: "Yanıtlarda Tartış",
+      description:
+          "İstediğin yoruma tıklayarak cevap yazabilir ve yorum sahibiyle tarif hakkında konuşabilirsin.",
       imageAsset: "assets/images/replyscreen.png",
     ),
     PageData(
       title: "Bolus Hesaplayıcı",
-      description: "Öğün listeni oluşturduktan sonra alman gereken insulini buradan hesapla.",
+      description:
+          "Öğün listeni oluşturduktan sonra gerekli alanları doldurup alman gereken insülini buradan hesapla.",
       imageAsset: "assets/images/bolus.png",
     ),
     PageData(
       title: "Bolus Raporların",
-      description: "Dilediğin tarih aralığındaki bolus raporlarını inceleyebilirsin.",
+      description:
+          "Dilediğin tarih aralığındaki bolus raporlarını inceleyebilirsin.",
       imageAsset: "assets/images/bolusreportdetails.png",
     ),
     PageData(
@@ -63,8 +72,9 @@ class InfoScreen extends StatelessWidget {
       imageAsset: "assets/images/pdfscreen.png",
     ),
     PageData(
-      title: "Eğitici Quizler",
-      description: "Birbirinden farklı eğitici ve öğretici quizleri çözüp bilgilerini test edebilirsin.",
+      title: "Eğlenceli Quizler",
+      description:
+          "Birbirinden farklı eğitici ve öğretici quizleri çözüp bilgilerini test edebilirsin.",
       imageAsset: "assets/images/quiz.png",
     ),
   ];
@@ -93,28 +103,27 @@ class InfoScreen extends StatelessWidget {
                 child: Column(
                   children: <Widget>[
                     Expanded(
-                      child: PageView.builder(
-                        controller: viewModel.pageController,
-                        onPageChanged: viewModel.onPageChanged,
-                        itemCount: pages.length,
-                        itemBuilder: (context, index) {
-                          return _buildPage(pages[index], context);
-                        },
+                      child: Center(
+                        child: PageView.builder(
+                          controller: viewModel.pageController,
+                          onPageChanged: viewModel.onPageChanged,
+                          itemCount: pages.length,
+                          itemBuilder: (context, index) {
+                            return _buildPage(pages[index], context);
+                          },
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          _buildBackButton(context, viewModel),
-                          ...List.generate(pages.length, (index) {
-                            return _buildIndicator(
-                                index == viewModel.currentPage);
-                          }),
-                          _buildForwardButton(context, viewModel)
-                        ],
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildBackButton(context, viewModel),
+                        ...List.generate(pages.length, (index) {
+                          return _buildIndicator(
+                              index == viewModel.currentPage);
+                        }),
+                        _buildForwardButton(context, viewModel)
+                      ],
                     ),
                   ],
                 ),
@@ -133,13 +142,13 @@ class InfoScreen extends StatelessWidget {
       children: <Widget>[
         Text(pageData.title,
             style: const TextStyle(
-              fontSize: 32,
+              fontSize: 28,
               fontWeight: FontWeight.bold,
               color: Colors.white,
             ),
             softWrap: true,
             textAlign: TextAlign.center),
-        const SizedBox(height: 20),
+        const SizedBox(height: 18),
         Text(
           pageData.description,
           textAlign: TextAlign.center,
